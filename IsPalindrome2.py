2dacd300913306c767f5807464d3e912dbba147c
lilly9117 <lilly9117@naver.com>
2020-12-22 12:46:04 +0900

    유효한 팰린드롬

--- IsPalindrome2.py
+++ IsPalindrome2.py
@@ -1,8 +1,10 @@
-#유효한 팰린드롬
+# 유효한 팰린드롬
 
-#주어진 문자열이 팰린드롬인지 확인하라. 대소문자를 구분하지 않으며, 영문자와 숫자만을 대상으로 한다.
+# 주어진 문자열이 팰린드롬인지 확인하라. 대소문자를 구분하지 않으며, 영문자와 숫자만을 대상으로 한다.
 
-#팰린드롬 : 앞뒤가 똑같은 문장 (뒤집어도 같은 말이 되는 단어 & 문장)
+# 팰린드롬 : 앞뒤가 똑같은 문장 (뒤집어도 같은 말이 되는 단어 & 문장)
+
+import re
 
 class Solution(object):
     def isPalindrome(self, s):
@@ -10,15 +12,9 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        strs = []
-        for char in s:
-            if char.isalnum(): #영문자, 숫자 여부 판별
-                strs.append(char.lower())
-                
-        while len(strs) > 1:
-            #pop으로 index 지정 
-            if strs.pop(0) != strs.pop(): # 맨마지막이랑 맨 첫번째거 비교!!!
-                return False
+        s = s.lower()
         
-        return True
+        s = re.sub('[^a-z0-9]', '', s)
+        
+        return s == s[::-1] #슬라이싱 이용 (뒤집어 버림)
             
